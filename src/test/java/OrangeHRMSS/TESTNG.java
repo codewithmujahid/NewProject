36a443d814edcac573b20dc3a0e39b12e0999657
codewithmujahid <codewithmujahid@gmail.com>
2026-02-04 15:24:06 +0530

    new changes are mode

--- src/test/java/OrangeHRMSS/TESTNG.java
+++ src/test/java/OrangeHRMSS/TESTNG.java
@@ -1,5 +1,35 @@
 package OrangeHRMSS;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.Color;
+
 public class TESTNG {
+	
+	WebDriver driver =new FirefoxDriver();
+	
+	public void Xass() throws InterruptedException {
+		driver.manage().window().maximize();
+		
+		 driver.get("https://saucelabs.com/");
+		 Thread.sleep(2000);
+		
+		WebElement element = driver.findElement(By.xpath("//*[text()='Try it free']"));
+		
+		
+		
+		
+		String colour=element.getCssValue("color");
+		String HXclour=Color.fromString(colour).asHex();
+		
+		assertEquals(HXclour, "#000000");
+		
+		
+		
+	}
 
 }
